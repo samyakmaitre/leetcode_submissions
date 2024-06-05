e40fcbf7ed3105e57cc6b7b874e0bd31deab0cca
Samyak Maitre <130790017+samyakmaitre@users.noreply.github.com>
2024-06-05 22:50:02 +0530

    Time: 2 ms (73.35%), Space: 45.2 MB (39.44%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        
+        int[][] dp = new int[m][n];
+        
+        // Base case for the bottom-right corner
+        dp[m-1][n-1] = Math.max(1, 1 - dungeon[m-1][n-1]);
+        
+        // Fill the last row
+        for (int i = m - 2; i >= 0; i--) {
+            dp[i][n-1] = Math.max(1, dp[i+1][n-1] - dungeon[i][n-1]);
+        }
+        
+        // Fill the last column
+        for (int j = n - 2; j >= 0; j--) {
+            dp[m-1][j] = Math.max(1, dp[m-1][j+1] - dungeon[m-1][j]);
+        }
+        
+        // Fill the rest of the table
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                int minHealthOnExit = Math.min(dp[i+1][j], dp[i][j+1]);
+                dp[i][j] = Math.max(1, minHealthOnExit - dungeon[i][j]);
+            }
+        }
+        
+        return dp[0][0];
+    }
+}
