142bfd5399649579823b8d33e14ca2ace022ee0d
Samyak Maitre <130790017+samyakmaitre@users.noreply.github.com>
2024-11-05 13:20:50 +0530

    Time: 2 ms (98.83%), Space: 44.8 MB (46.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,20 @@
 class Solution {
-  public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> numToIndex = new HashMap<>();
-
-    for (int i = 0; i < nums.length; ++i) {
-      if (numToIndex.containsKey(target - nums[i]))
-        return new int[] {numToIndex.get(target - nums[i]), i};
-      numToIndex.put(nums[i], i);
+    public int[] twoSum(int[] nums, int target){
+        //create a hashmap to store the the diff of target and current number
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        //Interate over the array
+        for (int i=0; i<nums.length; i++){
+    
+            //if the complement is in the map, we found the solution
+            if(map.containsKey(target - nums[i])){
+                return new int[] {map.get(target - nums[i]),i};
+            }
+            //Otherwise, add the current number and its index to the map
+            map.put(nums[i],i);
+        }
+        
+        //in case of no solution
+        throw new IllegalArgumentException("No solution");
     }
-
-    throw new IllegalArgumentException();
-  }
 }
